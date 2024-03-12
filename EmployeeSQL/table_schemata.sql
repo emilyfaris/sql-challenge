46fffcecdd21e288e50b8eca01b9f6142fad6da8
Emily Faris <emilyfaris@gmail.com>
2024-03-12 09:21:04 -0700

    updating sol

--- EmployeeSQL/table_schemata.sql
+++ EmployeeSQL/table_schemata.sql
@@ -1,60 +1,71 @@
-CREATE TABLE departments(
-	dept_no VARCHAR(4) PRIMARY KEY,
-	dept_name VARCHAR(255)
+﻿CREATE TABLE departments (
+    dept_no CHAR(4)   NOT NULL,
+    dept_name VARCHAR(30)   NOT NULL,
+    CONSTRAINT pk_departments PRIMARY KEY (
+        dept_no
+     )
 );
 
-SELECT *
-FROM departments;
-
-CREATE TABLE titles(
-	title_id VARCHAR(5) PRIMARY KEY,
-	title VARCHAR(255)
+CREATE TABLE titles (
+    title_id CHAR(5)   NOT NULL,
+    title VARCHAR(30)   NOT NULL,
+    CONSTRAINT pk_titles PRIMARY KEY (
+        title_id
+     )
 );
 
-SELECT *
-FROM titles; 
-
-DROP TABLE employees;
-
-CREATE TABLE employees(
-	emp_no INT PRIMARY KEY,
-	emp_title_id VARCHAR(5), 
-	birth_date VARCHAR(10),
-	first_name VARCHAR(255),
-	last_name VARCHAR(255),
-	sex CHAR(1),
-	hire_date VARCHAR(10),
-	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+CREATE TABLE employees (
+    emp_no INT   NOT NULL,
+    emp_title_id CHAR(5)   NOT NULL,
+    birth_date DATE   NOT NULL,
+    first_name VARCHAR(50)   NOT NULL,
+    last_name VARCHAR(50)   NOT NULL,
+    sex CHAR(1)   NOT NULL,
+    hire_date DATE   NOT NULL,
+    CONSTRAINT pk_employees PRIMARY KEY (
+        emp_no
+     )
 );
 
-SELECT *
-FROM employees;
-
-CREATE TABLE dept_emp(
-	emp_no INT,
-	dept_no VARCHAR(4),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+CREATE TABLE dept_emp (
+    emp_no INT   NOT NULL,
+    dept_no CHAR(4)   NOT NULL,
+    CONSTRAINT pk_dept_emp PRIMARY KEY (
+        emp_no, dept_no
+     )
 );
 
-SELECT *
-FROM dept_emp;
-
-CREATE TABLE dept_manager(
-	dept_no VARCHAR(4),
-	emp_no INT,
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+CREATE TABLE dept_manager (
+    dept_no CHAR(4)   NOT NULL,
+    emp_no INT   NOT NULL,
+    CONSTRAINT pk_dept_manager PRIMARY KEY (
+        emp_no
+     )
 );
 
-SELECT *
-FROM dept_manager;
-
-CREATE TABLE salaries(
-	emp_no INT,
-	salary INT,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+CREATE TABLE salaries (
+    emp_no INT   NOT NULL,
+    salary INT   NOT NULL,
+    CONSTRAINT pk_salaries PRIMARY KEY (
+        emp_no
+     )
 );
 
-SELECT *
-FROM salaries;
+ALTER TABLE employees ADD CONSTRAINT fk_employees_emp_title_id FOREIGN KEY(emp_title_id)
+REFERENCES titles (title_id);
+
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
+
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
+
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE salaries ADD CONSTRAINT fk_salaries_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
